b5bdc0e64b9221539c5041da9358cac250b0d0d8
Manaswini Krishna T <113501692+user-130613@users.noreply.github.com>
2023-12-29 18:03:26 +0530

    Add files via upload

---
+++ Simple_calculator.py
@@ -0,0 +1,42 @@
+# Displaying the operations that can be done by calculator
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+#Enter the operation index choice from the display
+choice = input("Enter choice (1/2/3/4): ")
+
+#Entering the input numbers on which operations need to be performed
+if choice in ['1', '2', '3', '4']:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    
+    #Addition
+    if choice == '1':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+        
+    #Substraction
+    elif choice == '2':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+
+    #Multiplication
+    elif choice == '3':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+
+    #Division
+    elif choice == '4':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Cannot divide by zero.")
+            
+#If the opertion is not available on the display
+else:
+    print("Invalid input. Please enter a valid choice (1/2/3/4).")
+
