e7dd122486e6371f986079d8ea04458ff1d88d2e
Manaswini Krishna T <113501692+user-130613@users.noreply.github.com>
2023-12-29 18:05:33 +0530

    Update Simple_calculator.py

--- Simple_calculator.py
+++ Simple_calculator.py
@@ -6,33 +6,33 @@ print("3. Multiplication")
 print("4. Division")
 
 #Enter the operation index choice from the display
-choice = input("Enter choice (1/2/3/4): ")
+choice=input("Enter choice (1/2/3/4): ")
 
 #Entering the input numbers on which operations need to be performed
-if choice in ['1', '2', '3', '4']:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+if choice in['1','2','3','4']:
+    num1=float(input("Enter first number: "))
+    num2=float(input("Enter second number: "))
     
     #Addition
-    if choice == '1':
-        result = num1 + num2
-        print(f"{num1} + {num2} = {result}")
+    if choice=='1':
+        result=num1+num2
+        print(f"{num1}+{num2}={result}")
         
     #Substraction
-    elif choice == '2':
-        result = num1 - num2
-        print(f"{num1} - {num2} = {result}")
+    elif choice=='2':
+        result=num1-num2
+        print(f"{num1}-{num2}={result}")
 
     #Multiplication
-    elif choice == '3':
-        result = num1 * num2
-        print(f"{num1} * {num2} = {result}")
+    elif choice=='3':
+        result=num1*num2
+        print(f"{num1}*{num2}={result}")
 
     #Division
-    elif choice == '4':
-        if num2 != 0:
-            result = num1 / num2
-            print(f"{num1} / {num2} = {result}")
+    elif choice=='4':
+        if num2!=0:
+            result=num1/num2
+            print(f"{num1}/{num2}={result}")
         else:
             print("Cannot divide by zero.")
             
